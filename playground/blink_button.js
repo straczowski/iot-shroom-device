a75c2473c33a507d9e909946264a1713a6837cdc
Raoul Straczowski <raoulstraczowski@googlemail.com>
2017-12-22 18:00:51 +0100

    new

--- playground/blink_button.js
+++ playground/blink_button.js
@@ -1,19 +1,37 @@
 var gpio = require("gpio");
+var gpio22, gpio4, intervalTimer;
  
-// Calling export with a pin number will export that header and return a gpio header instance 
-var gpio4 = gpio.export(11, {
-   // When you export a pin, the default direction is out. This allows you to set 
-   // the pin value to either LOW or HIGH (3.3V) from your program. 
-   direction: 'out',
- 
-   // set the time interval (ms) between each read when watching for value changes 
-   // note: this is default to 100, setting value too low will cause high CPU usage 
-   // interval: 200,
- 
-   // Due to the asynchronous nature of exporting a header, you may not be able to 
-   // read or write to the header right away. Place your logic in this ready 
-   // function to guarantee everything will get fired properly 
+// Flashing lights if LED connected to GPIO22 
+gpio22 = gpio.export(11, {
    ready: function() {
-      console.log("ready function exec");
+      intervalTimer = setInterval(function() {
+         gpio22.set();
+         setTimeout(function() { gpio22.reset(); }, 500);
+      }, 1000);
    }
 });
+ 
+// Lets assume a different LED is hooked up to pin 4, the following code  
+// will make that LED blink inversely with LED from pin 22  
+gpio4 = gpio.export(12, {
+   ready: function() {
+      // bind to gpio22's change event 
+      gpio22.on("change", function(val) {
+         gpio4.set(1 - val); // set gpio4 to the opposite value 
+      });
+   }
+});
+ 
+// reset the headers and unexport after 10 seconds 
+setTimeout(function() {
+   clearInterval(intervalTimer);          // stops the voltage cycling 
+   gpio22.removeAllListeners('change');   // unbinds change event 
+   gpio22.reset();                        // sets header to low 
+   gpio22.unexport();                     // unexport the header 
+   
+   gpio4.reset();
+   gpio4.unexport(function() {
+      // unexport takes a callback which gets fired as soon as unexporting is done 
+      process.exit(); // exits your node program 
+   });
+}, 10000)
